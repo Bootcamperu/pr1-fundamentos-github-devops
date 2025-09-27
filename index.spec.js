d6d6110827527b4d11400578902dab5bcdf0c9b5
bootcamperu <rpabootcamperu@gmail.com>
2025-09-27 16:26:38 -0500

    Pruebas

--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,7 @@
 const dice = require("./index");
-test("Returns a random number between 1 and 6", () => {
+test("Retorme un numero entre el 1 y 6", () => {
     const rollResult = dice.roll();
-    console.log(`result was ${rollResult}`);
+    console.log(`Resultado es ${rollResult}`);
     expect(rollResult).toBeGreaterThanOrEqual(1);
     expect(rollResult).toBeLessThanOrEqual(6);
 });
